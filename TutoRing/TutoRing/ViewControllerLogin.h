8126033b9c86e309acbc65f3a5d5f8254f128fa9
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2015-10-03 18:34:46 -0500

    Log In and Sign Up develop
    Log In and Sign Up interfaces where started, but only on storyboard.

---
+++ TutoRing/TutoRing/ViewControllerLogin.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerLogin.h
+//  TutoRing
+//
+//  Created by Alejandro Zamudio Guajardo on 10/3/15.
+//  Copyright © 2015 OmarCarreon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerLogin : UIViewController
+
+@end
