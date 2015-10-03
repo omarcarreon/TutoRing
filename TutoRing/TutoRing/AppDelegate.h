bf3f1fff90ac8db0fd5d29942ce36e0d08a2e620
omarcarreon <oomarcarreon@gmail.com>
2015-10-03 14:27:41 -0500

    initial commit

---
+++ TutoRing/TutoRing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TutoRing
+//
+//  Created by Omar Carreon on 03/10/15.
+//  Copyright © 2015 OmarCarreon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TutoRing/TutoRing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TutoRing
+//
+//  Created by Omar Carreon on 03/10/15.
+//  Copyright © 2015 OmarCarreon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
