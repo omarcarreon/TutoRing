5a0b9c407d638725ae6e73c88b45f779566c806f
elielr01 <eli.elr01@gmail.com>
2015-10-04 04:39:39 -0500

    table view con botones correctos

--- TutoRing/TutoRing/LeftMenuViewController.m
+++ TutoRing/TutoRing/LeftMenuViewController.m
@@ -13,7 +13,7 @@
 #import "SlideNavigationContorllerAnimatorScaleAndFade.h"
 #import "SlideNavigationContorllerAnimatorSlideAndFade.h"
 
-@interface LeftMenuViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface LeftMenuViewController () 
 
 @property (nonatomic, strong) NSMutableArray *options;
 
@@ -38,32 +38,65 @@
     self.imavProfileImage.clipsToBounds = YES;
     
     self.options = [[NSMutableArray alloc] initWithObjects:@"Profile", @"Settings", @"Log out", nil];
+    
+    _configMenu.backgroundColor = [UIColor clearColor];
+    _configMenu.delegate = self;
+    [_configMenu reloadData];
+    
+    _configMenu.dataSource = self;
+    _configMenu.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
 
-#pragma mark - Table implementation
+#pragma mark - UITableView Delegate & Datasrouce 
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
     // Return the number of sections.
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 3;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"menuOption"];
+    UITableViewCell *cell = [_configMenu dequeueReusableCellWithIdentifier:@"menu"];
     
-    NSString *Texto = self.options[indexPath.row];
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"Profile";
+    }
+    else if (indexPath.row == 1) {
+        cell.textLabel.text = @"Settings";
+    }
+    else {
+        cell.textLabel.text = @"Log Out";
+    }
     
-    cell.textLabel.text = Texto;
+    cell.backgroundColor = [self colorFromHexString:@"#CC594C"];
+    cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    cell.textLabel.textColor = [UIColor whiteColor];
+    
+    cell.layer.cornerRadius = 8.0f;
+    
+    
+    
     
     return cell;
 }
+
+- (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
 
 @end
